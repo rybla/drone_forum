4ceaffcca05740b2e85f2185238a6ebf70a26014
Henry Blanchette <henry@rybl.net>
2025-06-03 14:35:17 -0400

    checkpoint before trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { Post } from "@/types";
+import { type Post } from "@/types";
 
 export default async function Home() {
   const posts_response = await fetch(`${process.env.URL}/api/get_posts`);
-  const posts: Post[] = await posts_response.json();
+  const posts: Post[] = (await posts_response.json()).posts;
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -16,10 +16,20 @@ export default async function Home() {
         <div>
           <a href="/new_post">new_post</a>
         </div>
-        <div>
-          <pre>{JSON.stringify(posts, null, 4)}</pre>
+        <div className="posts">
+          {posts.map((post, i) => (
+            <Post key={i} post={post} />
+          ))}
         </div>
       </main>
+    </div>
+  );
+}
+
+function Post(props: { post: Post }) {
+  return (
+    <div className="Post">
+      <pre>{JSON.stringify(props.post, null, 4)}</pre>
     </div>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 export type User = {
   id: string;
   name: string;
-  joinDate: Date;
   description: string;
   roles: string[];
 };
